cea142e13e7d473d26f2c21103bf731f0eee9e8d
Benjamin <benjiprores@gmail.com>
2025-01-31 11:08:20 +0000

    added sections to homepage

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero.jsx';
+import CustomerReviews from "./CustomerReviews.jsx";
+import Footer from "./Footer.jsx";
+import Subscribe from "./Subscribe.jsx";
+import SuperQuality from "./SuperQuality.jsx";
+import SpecialOffers from "./SpecialOffers.jsx";
+import Services from "./Services.jsx";
+import PopularProducts from "./PopularProducts.jsx";
+
+export {
+    Hero,
+    CustomerReviews,
+    Footer,
+    Subscribe,
+    SuperQuality,
+    SpecialOffers,
+    Services,
+    PopularProducts,
+}
