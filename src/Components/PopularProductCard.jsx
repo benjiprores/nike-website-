a6fe681b3d98cad736172f8fb9c7156f5a7bb6d6
Benjamin <benjiprores@gmail.com>
2025-02-12 08:19:38 +0000

    popular product section done

--- src/Components/PopularProductCard.jsx
+++ src/Components/PopularProductCard.jsx
@@ -1,6 +1,18 @@
-const PopularProductCard = () => {
+import {star} from "../assets/icons/index.js";
+
+const PopularProductCard = ({imgURL, name, price}) => {
     return (
-        <div>PopularProductCard</div>
+        <div className="flex flex-1 flex-col w-full max-sm:w-full">
+            <img src={imgURL} alt={name} className="w-[280px] h-[280x]"/>
+            <div className="mt-8 justify-start gap-2.5">
+                <img src={star} alt="Rating" width={24} height={24}/>
+                <p className="font-montserrat leadiing-normal text-xl text-slate-gray">4.5</p>
+                <h3 className="mt-2 text-lg leading-normal font-palanquin font-semibold">{name}</h3>
+                <p className="mt-2 font-montserrat font-semibold text-coral-red text-2xl leading-normal">{price}</p>
+            </div>
+        </div>
+
+
     )
 }
 export default PopularProductCard
